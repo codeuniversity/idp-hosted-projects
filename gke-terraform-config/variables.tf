656ed7239dc973c368acb8275b5d4fc3c382eb90
Laurin-Notemann <laurin.notemann@gmail.com>
2024-05-14 16:13:11 +0200

    feat: split kubernetes and gke creation

--- gke-terraform-config/variables.tf
+++ gke-terraform-config/variables.tf
@@ -23,27 +23,3 @@ variable "max_node_count" {
 variable "service_account" {
   type = string
 }
-
-variable "namedotcom_username" {
-  type = string
-}
-
-variable "namedotcom_token" {
-  type = string
-}
-
-variable "idp_domain_name" {
-  type        = string
-  description = "The domain name (e.g. code.berlin)"
-}
-
-variable "idp_domain_host" {
-  type        = string
-  description = "The Subdomain, needs to be a wildcard (e.g. '*.idp' which means test.idp.code.berlin is a valid domain)"
-  default     = "*.idp"
-}
-
-variable "certificate_issuer_email" {
-  type        = string
-  description = "This is the email that is used to create the LetsEncrypt certificate"
-}
